69d991b53e2f0109800f85e702c0aba9814b4a26
DaviLikesan123 <davidlimboksiok@gmail.com>
2021-09-07 17:44:31 -0300

    Nac

---
+++ app/src/main/java/br/com/fiap/foodhealth/SplashActivity.kt
@@ -0,0 +1,34 @@
+package br.com.fiap.foodhealth
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import br.com.fiap.foodhealth.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Tirar a actionBar
+        supportActionBar?.hide()
+
+        //Animação
+        binding.clSplash.animate().setDuration(1500).alpha(1f).withEndAction{
+            val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/br/com/fiap/foodhealth/Utils.kt
@@ -0,0 +1,10 @@
+package br.com.fiap.foodhealth
+
+import android.view.View
+
+fun View.fadeIn(){
+    this.apply{}
+    animate()
+
+
+}
