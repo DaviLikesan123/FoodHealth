7cf4ab12d364a7f9b0b20b609a4bfbc90eb65482
DaviLikesan123 <davidlimboksiok@gmail.com>
2021-09-11 19:19:18 -0300

    Form ready

--- app/src/main/java/br/com/fiap/foodhealth/MainActivity.kt
+++ app/src/main/java/br/com/fiap/foodhealth/MainActivity.kt
@@ -2,17 +2,34 @@ package br.com.fiap.foodhealth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import br.com.fiap.foodhealth.databinding.ActivityMainBinding
+import br.com.fiap.foodhealth.fragments.ImcFragment
+import br.com.fiap.foodhealth.fragments.ListFragment
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    private val ImcFragment = ImcFragment()
+    private val ListFragment = ListFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        replaceFragment(ImcFragment)
 
+        binding.bottomNavigation.setOnNavigationItemReselectedListener {
+            when(it.itemId){
+                R.id.ic_bmi -> replaceFragment(ImcFragment)
+                R.id.ic_list -> replaceFragment(ListFragment)
+            }
+        }
+    }
 
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_container,fragment)
+        transaction.commit()
     }
 }
