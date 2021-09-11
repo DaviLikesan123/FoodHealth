3c1f08a566397a60907bed9d2c3727729417ea0b
DaviLikesan123 <davidlimboksiok@gmail.com>
2021-09-11 14:53:46 -0300

    Fixing mainScreen

--- app/src/main/java/br/com/fiap/foodhealth/SplashActivity.kt
+++ app/src/main/java/br/com/fiap/foodhealth/SplashActivity.kt
@@ -23,7 +23,7 @@ class SplashActivity : AppCompatActivity() {
         //Animação
         binding.clSplash.animate().setDuration(1500).alpha(1f).withEndAction{
             val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
+            startActivity(intent)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
         }
