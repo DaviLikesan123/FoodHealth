7e3bba45aa50739495f6a7cb7b9f6b57474b5446
DaviLikesan123 <davidlimboksiok@gmail.com>
2021-09-12 12:48:02 -0300

    Doing the IMC calc

--- app/src/main/java/br/com/fiap/foodhealth/fragments/ImcFragment.kt
+++ app/src/main/java/br/com/fiap/foodhealth/fragments/ImcFragment.kt
@@ -1,20 +1,37 @@
 package br.com.fiap.foodhealth.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import br.com.fiap.foodhealth.R
+import br.com.fiap.foodhealth.databinding.FragmentImcBinding
+import com.google.android.material.textfield.TextInputEditText
 
 class ImcFragment : Fragment() {
 
+    private lateinit var binding: FragmentImcBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imc, container, false)
+    ): View {
+
+        binding = FragmentImcBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btCalcular.setOnClickListener {
+
+        }
+
     }
 }
+
+
