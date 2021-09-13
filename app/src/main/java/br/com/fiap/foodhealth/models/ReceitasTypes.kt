6f061777353be56a887dfe852922e44180062b6e
Giuzntt <giuzntt@gmail.com>
2021-09-12 22:58:48 -0300

    Aplicando a API

---
+++ app/src/main/java/br/com/fiap/foodhealth/models/ReceitasTypes.kt
@@ -0,0 +1,12 @@
+package br.com.fiap.foodhealth.models
+
+    data class Receitas(
+        val name: String,
+        val calories: Recipes
+    )
+    data class Recipes(
+    val calories: String
+    )
+    data class Results(
+        val name: String
+            )
